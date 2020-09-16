a91e8b3d655226327771bfafd043fb3b3a7e2acf
Ramy <ramyadeeb@gmail.com>
2020-09-15 21:32:59 -0700

    cleanup

--- htdocs/index.php
+++ htdocs/index.php
@@ -86,17 +86,17 @@
 				<div class="pillars">
 					<p>How we invest</p>
           <ol>
-				    <li>We focus on early-seed rounds. It’s what we know and love and where we provide the most value </li>
-				    <li>We are drawn to fast-moving founders with industry insights and a strong engineering culture</li>
-            <li>We are comfortable with a SAFE plus pro-rata or a priced round and can lead or participate.</li>
+				    <li>We focus on seed rounds. It’s what we know and love and where we provide the most value </li>
+				    <li>We are drawn to founders with industry insights and a strong engineering culture</li>
+            <li>We are flexible on the mechanics. We can use a SAFE plus pro-rata or a priced round, and can lead or participate.</li>
           </ol>
         </div>
 				<div class="pillars MID">
           <p>How we operate</p>
           <ol>
             <li>We don't take board seats.  We prefer to be in your speed dial and get the late-night call</li>
-            <li>We help you recruit an A+ team </li>
-            <li>We make intros that matter, and particularly help with fundraising.
+            <li>We make intros that matter</li>
+            <li>We help you recruit an A+ team and assist with fundraising</li>
           </ol>
         </div>
 			</div>
